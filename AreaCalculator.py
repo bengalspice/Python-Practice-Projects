f6207e12ace72d23fa6b29be5917003a26977653
Sharmeen <32082105+bengalspice@users.noreply.github.com>
2018-03-02 00:10:21 -0500

    Update AreaCalculator.py

--- AreaCalculator.py
+++ AreaCalculator.py
@@ -21,13 +21,26 @@ sleep(1)
 
 hint = "Don't forget to include the correct units! \nExiting..."
 
-option = raw_input("Enter C for Circle or T for Triangle: ")
+option = raw_input("Enter C for Circle, R for Rectangle, S for Square, T for Triangle: ")
 option = option.upper()
 
 if option == 'C':
   radius = float(raw_input("Enter radius: "))
   area = pi * radius ** 2
   print "The pie is baking..."
+  sleep(1)
+  print ("Area: %.2f \n%s" % (area, hint))
+elif option == 'R':
+  length = float(raw_input("Enter the length: "))
+  width = float(raw_input("Enter the width: "))
+  area = length * width
+  print "What do you call a crushed angle?"
+  sleep(1)
+  print ("Area: %.2f \n%s" % (area, hint))
+elif option == 'S':
+  side = float(raw_input("Enter the side: "))
+  area = side ** 2
+  print "Hip to be square..."
   sleep(1)
   print ("Area: %.2f \n%s" % (area, hint))
 elif option == 'T':
